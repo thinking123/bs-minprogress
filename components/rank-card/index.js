3a58e8077d68ae7dd3b64f2889902b10d9e11de9
7ab_100009078201_ThinKing <7ab_100009078201_ThinKing@git.cloud.tencent.com>
2019-01-28 22:16:22 +0800

    rank list UI

---
+++ components/rank-card/index.js
@@ -0,0 +1,26 @@
+Component({
+
+    behaviors: [],
+
+    properties: {
+        visible: {
+            type: Boolean,
+            value: false,
+            observer(newVal, oldVal, changedPath) {
+                console.log('visible change ' , newVal , oldVal ,changedPath)
+            }
+        }
+    },
+    data: {},
+    methods: {
+        handleHide() {
+            this.triggerEvent('hidetap')
+        },
+        handleOutHide(e) {
+            if (e && e.target && e.target.id === 'mask') {
+                this.triggerEvent('hidetap')
+            }
+        }
+    }
+
+})
