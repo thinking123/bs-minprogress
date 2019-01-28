67d5493362b7c42f67ef67a3f5e4b6a266ad4fdb
7ab_100009078201_ThinKing <7ab_100009078201_ThinKing@git.cloud.tencent.com>
2019-01-29 00:34:39 +0800

    gz list UI

---
+++ pages/attention/index.js
@@ -0,0 +1,42 @@
+// pages/attention/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        attentionList: [
+            {
+                name:'历史的',
+                num:'007'
+            },
+            {
+                name:'历史的',
+                num:'007'
+            },
+            {
+                name:'历史的',
+                num:'007'
+            },
+            {
+                name:'历史的',
+                num:'007'
+            },
+            {
+                name:'历史的',
+                num:'007'
+            },
+            {
+                name:'历史的',
+                num:'007'
+            },
+            {
+                name:'历史的',
+                num:'007'
+            }
+        ]
+    },
+    handleReturn() {
+
+    }
+})
