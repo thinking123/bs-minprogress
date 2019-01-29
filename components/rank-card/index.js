229f043527efae0fbd82b3772bb3c8a61f8612c5
7ab_100009078201_ThinKing <7ab_100009078201_ThinKing@git.cloud.tencent.com>
2019-01-29 23:41:56 +0800

    rank list  UI

--- components/rank-card/index.js
+++ components/rank-card/index.js
@@ -9,35 +9,47 @@ Component({
             type: String,
             value: '',
             observer(level) {
-
-                return
+                return;
+                if (level.length === 0) {
+                    return
+                }
                 wx.nextTick(() => {
+                    // this.isIntersection = false
+                    // const _observer = this.createIntersectionObserver(this, {
+                    //     left: 0,
+                    //     top: 0
+                    // })
+                    // this.setData({
+                    //     _observer: _observer
+                    // })
+                    // this.data._observer
+                    //     .relativeTo('.rank-card-disk')
+                    //     .observe('.disk-dot', (res) => {
+                    //         console.log('Intersection');
+                    //         // wx.nextTick(()=>{
+                    //         //     this.isIntersection = true
+                    //         //     this.data._observer.disconnect()
+                    //         // })
+                    //         this.isIntersection = true
+                    //         this.data._observer.disconnect()
+                    //     })
+
                     let q = this.createSelectorQuery()
-                    this._observer = this.createIntersectionObserver(this)
-                    // let step = 0.02
-                    let initRatio = 0.5
-                    // this.isSeparate = false
-                    this.isIntersection = false
+                    const dot = q.select("#disk-dot")
+                    const rank = q.select('#rank-card-disk')
 
-                    this._observer
-                        .relativeTo('#rank-card-wrap')
-                        .observe('#disk-dot', (res) => {
-                            console.log('Intersection');
-                            this.isIntersection = true
-                        })
-
-                    if (q && this._observer) {
-                        const dot = q.select("#disk-dot")
-                        const rank = q.select('#rank-card-wrap')
-                        if(dot && rank){
-                            this.adjust(dot ,rank , initRatio)
-                        }
+                    if (dot && rank) {
+                        rank.boundingClientRect(rect => {
+                            const w = rect.width
+                            const h = rect.height
+                            this.initW = w
+                            this.initH = h
+                            this.adjust3(w, dot, rank)
+                        }).exec()
 
                     }
 
                 })
-
-
             }
         },
         name: String,
@@ -46,11 +58,12 @@ Component({
         vote: Number,
         info: Object
     },
+    detached() {
+        if (this.data._observer) this.data._observer.disconnect()
+    },
     computed: {
         poleTransform() {
             let angle = this.data.playing ? '7deg' : '0'
-
-            console.log('transofrm roate', `rotate(${angle})`)
             return `rotate(${angle})`
         },
         headerSrc() {
@@ -72,63 +85,134 @@ Component({
     },
     data: {
         playing: false,
-        diskWidth: '100rpx',
-        diskHeight: '100rpx',
-        diskRadius: '100rpx',
-    },
-    onUnload() {
-        if (this._observer) this._observer.disconnect()
+        diskWidth: '10rpx',
+        diskHeight: '10rpx',
+        diskRadius: '10rpx',
+        _observer: {}
     },
     methods: {
-        adjust(dot ,rank , ratio) {
-            // const dot = q.select("#disk-dot")
-            // const rank = q.select('#rank-card-wrap')
-            let step = 0.02
-            ratio += step
-            if (ratio > 0.95) {
+
+        adjust2(rank, width) {
+            return
+            let step = 10
+            width += step
+            if (width > 1000) {
                 return
             }
-            rank.boundingClientRect(rect => {
-                console.log('w , h', rect.width, rect.height)
-                const w = rect.width
-                const h = rect.height
+            width = width * 2 + 'rpx'
+            this.setData({
+                diskWidth: width,
+                diskHeight: width,
+                diskRadius: width,
+            }, () => {
+                setTimeout(() => {
+                    rank.boundingClientRect(rect => {
+                        console.log('rect width', rect.width)
+                        this.adjust(rank, rect.width)
+                    }).exec()
+                }, 50)
+                // wx.nextTick(() => {
+                //     rank.boundingClientRect(rect => {
+                //         console.log('rect width' , rect.width)
+                //         this.adjust( rank, rect.width)
+                //     }).exec()
+                // })
+            })
 
-                // let diskWidth = w * 0.7522 * 2;
-                let diskWidth = w * ratio * 2;
-                diskWidth += 'rpx'
-                console.log('diskWidth : ', diskWidth)
-
-                this.setData({
-                    diskWidth: diskWidth,
-                    diskHeight: diskWidth,
-                    diskRadius: diskWidth,
-                })
-
-
-
-                // setTimeout(()=>{
-                //     if (!this.isIntersection) {
-                //         this.adjust(dot ,rank , ratio)
-                //     }
-                // } , 200)
-                wx.nextTick(() => {
-                    if (!this.isIntersection) {
-                        this.adjust(dot ,rank , ratio)
-                    }
-                })
-            }).exec()
 
         },
-        getRect() {
+        adjust3(rW, dot, rank) {
+            let step = 2
+            let width = rW +  step
+            const w = width
+            if (width > 1000) {
+                return
+            }
+            width = width  + 'px'
+            this.setData({
+                diskWidth: width,
+                diskHeight: width,
+                diskRadius: width,
+            }, () => {
+                wx.nextTick(()=>{
+                    this.checkIntersection(dot, rank).then(([isInter , dW]) => {
+                        if (!isInter) {
+                            this.adjust3(dW, dot, rank)
+                        } else {
+                            console.log('end adjust')
+                        }
+                    })
+                })
+
+            })
+        },
+        getRect(node) {
             return new Promise((res, rej) => {
-                rank.boundingClientRect(rect => {
+                node.boundingClientRect(rect => {
                     res(rect)
                 }).exec()
             })
         },
+        checkIntersection(dot, rank) {
+
+            return Promise.all([this.getRect(dot), this.getRect(rank)]).then(rect => {
+                const dotR = rect[0]
+                const rankR = rect[1]
+                const dX = dotR.left
+                const dY = dotR.top
+                const rX = rankR.left + rankR.width
+                const rY = rankR.top + rankR.height
+
+                let isInter = false
+                isInter = dX < rX && dY < rY
+
+                console.log('dot :', dX, dY, ' rank ', rX, rY)
+                // return new Promise((res, rej) => {
+                //     res(isInter)
+                // })
+
+                return [isInter , rankR.width]
+            })
+        },
+        adjust1(dot, rank, ratio) {
+            // let step = 0.02
+            let step = 0.02
+            ratio += step
+            if (ratio > 0.9) {
+                return
+            }
+            console.log(this.data.level, ' ', ratio)
+
+            let diskWidth = this.initW * ratio * 2;
+            diskWidth += 'rpx'
+
+            this.setData({
+                diskWidth: diskWidth,
+                diskHeight: diskWidth,
+                diskRadius: diskWidth,
+            }, () => {
+                wx.nextTick(() => {
+                    if (!this.isIntersection) {
+                        this.adjust(dot, rank, ratio)
+                    }
+                })
+            })
+            // wx.nextTick(() => {
+            //     if (!this.isIntersection) {
+            //         this.adjust(dot, rank, ratio)
+            //     }
+            // })
+        },
+        // getRect() {
+        //     return new Promise((res, rej) => {
+        //         rank.boundingClientRect(rect => {
+        //             res(rect)
+        //         }).exec()
+        //     })
+        // },
         getIntersection() {
             return new Promise((res, rej) => {
-                this._observer
+                this.data._observer
                     .relativeTo('#rank-card-wrap')
                     .observe('#disk-dot', (res) => {
                         console.log('Intersection');
