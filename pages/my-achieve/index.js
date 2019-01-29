8f78676643e9512de04fc37adc85df9d0ae9d0eb
7ab_100009078201_ThinKing <7ab_100009078201_ThinKing@git.cloud.tencent.com>
2019-01-29 10:26:45 +0800

    achieve page UI

---
+++ pages/my-achieve/index.js
@@ -0,0 +1,55 @@
+// pages/my-achieve/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        achieveList:[
+            {
+                name:'我为原创代言',
+                progress:"60%",
+                text:'上传原创歌曲'
+            },
+            {
+                name:'我为原创代言',
+                progress:"80%",
+                text:'上传原创歌曲'
+            },
+            {
+                name:'我为原创代言',
+                progress:"60%",
+                text:'上传原创歌曲'
+            },
+            {
+                name:'我为原创代言',
+                progress:"60%",
+                text:'上传原创歌曲'
+            },
+            {
+                name:'我为原创代言',
+                progress:"60%",
+                text:'上传原创歌曲'
+            },
+            {
+                name:'我为原创代言',
+                progress:"60%",
+                text:'上传原创歌曲'
+            },
+            {
+                name:'我为原创代言',
+                progress:"60%",
+                text:'上传原创歌曲'
+            },
+            {
+                name:'我为原创代言',
+                progress:"20%",
+                text:'上传原创歌曲'
+            },
+
+        ]
+    },
+    handleReturn() {
+
+    }
+})
