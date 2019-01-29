3377f3853a20da216f4cfdff4ad38863b09c56eb
7ab_100009078201_ThinKing <7ab_100009078201_ThinKing@git.cloud.tencent.com>
2019-01-29 18:02:49 +0800

    rank list page

--- components/rank-card/index.js
+++ components/rank-card/index.js
@@ -1,18 +1,154 @@
+const computedBehavior = require('miniprogram-computed')
 Component({
 
-    behaviors: [],
+    behaviors: [computedBehavior],
+    externalClasses: ['rank-card-external'],
 
     properties: {
-        visible: {
-            type: Boolean,
-            value: false,
-            observer(newVal, oldVal, changedPath) {
-                console.log('visible change ', newVal, oldVal, changedPath)
+        level: {
+            type: String,
+            value: '',
+            observer(level) {
+
+                return
+                wx.nextTick(() => {
+                    let q = this.createSelectorQuery()
+                    this._observer = this.createIntersectionObserver(this)
+                    // let step = 0.02
+                    let initRatio = 0.5
+                    // this.isSeparate = false
+                    this.isIntersection = false
+
+                    this._observer
+                        .relativeTo('#rank-card-wrap')
+                        .observe('#disk-dot', (res) => {
+                            console.log('Intersection');
+                            this.isIntersection = true
+                        })
+
+                    if (q && this._observer) {
+                        const dot = q.select("#disk-dot")
+                        const rank = q.select('#rank-card-wrap')
+                        if(dot && rank){
+                            this.adjust(dot ,rank , initRatio)
+                        }
+
+                    }
+
+                })
+
+
             }
-        }
+        },
+        name: String,
+        school: String,
+        song: String,
+        vote: Number,
+        info: Object
     },
-    data: {},
+    computed: {
+        poleTransform() {
+            let angle = this.data.playing ? '7deg' : '0'
+
+            console.log('transofrm roate', `rotate(${angle})`)
+            return `rotate(${angle})`
+        },
+        headerSrc() {
+            return `./${this.data.level}.png`
+        },
+        playStatusSrc() {
+            const url = this.data.playing ? 'playing' : 'play'
+            return `./${url}.png`
+        },
+        // diskWidth(){
+        //     let rank = wx.createSelectorQuery().select('#rank-card-wrap')
+        //     rank.boundingClientRect(rect => {
+        //         console.log('w , h',rect.height, rect.width)
+        //         const w = rect.width
+        //         res(rect)
+        //     }).exec()
+        //     return `./${this.data.level}.png`
+        // },
+    },
+    data: {
+        playing: false,
+        diskWidth: '100rpx',
+        diskHeight: '100rpx',
+        diskRadius: '100rpx',
+    },
+    onUnload() {
+        if (this._observer) this._observer.disconnect()
+    },
     methods: {
+        adjust(dot ,rank , ratio) {
+            // const dot = q.select("#disk-dot")
+            // const rank = q.select('#rank-card-wrap')
+            let step = 0.02
+            ratio += step
+            if (ratio > 0.95) {
+                return
+            }
+            rank.boundingClientRect(rect => {
+                console.log('w , h', rect.width, rect.height)
+                const w = rect.width
+                const h = rect.height
+
+                // let diskWidth = w * 0.7522 * 2;
+                let diskWidth = w * ratio * 2;
+                diskWidth += 'rpx'
+                console.log('diskWidth : ', diskWidth)
+
+                this.setData({
+                    diskWidth: diskWidth,
+                    diskHeight: diskWidth,
+                    diskRadius: diskWidth,
+                })
+
+
+
+                // setTimeout(()=>{
+                //     if (!this.isIntersection) {
+                //         this.adjust(dot ,rank , ratio)
+                //     }
+                // } , 200)
+                wx.nextTick(() => {
+                    if (!this.isIntersection) {
+                        this.adjust(dot ,rank , ratio)
+                    }
+                })
+            }).exec()
+
+        },
+        getRect() {
+            return new Promise((res, rej) => {
+                rank.boundingClientRect(rect => {
+                    res(rect)
+                }).exec()
+            })
+        },
+        getIntersection() {
+            return new Promise((res, rej) => {
+                this._observer
+                    .relativeTo('#rank-card-wrap')
+                    .observe('#disk-dot', (res) => {
+                        console.log('Intersection');
+                        // this.isSeparate = true
+                        this.isIntersection = true
+                    })
+            })
+        },
+        handlePlay() {
+            this.triggerEvent('handlePlay')
+            this.setData({
+                playing: !this.data.playing
+            })
+        },
+        handleFollow() {
+            this.triggerEvent('handleFollow')
+        },
+        handleVote() {
+            this.triggerEvent('handleVote')
+        },
         handleHide() {
             this.triggerEvent('hidetap')
         },
--- pages/rank-list/index.js
+++ pages/rank-list/index.js
@@ -1,66 +1,166 @@
 // pages/rank-list/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        showCityList: [
+            '北京',
+            '上海',
+            '广州',
+            '深圳'
+        ],
+        gold:{
+            name:'定积分',
+            school:'北京大学',
+            region:'北京赛区',
+            song:'什么歌曲',
+            vote:881,
+            followed:false
+        },
+        rankListTopThree:[
+            {
+                name:'定积分',
+                school:'北京大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:881,
+                followed:false
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京的赛区',
+                song:'什么歌曲',
+                vote:822,
+                followed:true
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:821,
+                followed:false
+            },
+        ],
+        rankListOther:[
+            {
+                name:'定积分',
+                school:'北京大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:881,
+                followed:false
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京的赛区',
+                song:'什么歌曲',
+                vote:822,
+                followed:false
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:821,
+                followed:false
+            },
+            {
+                name:'定积分',
+                school:'北京大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:881,
+                followed:true
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京的赛区',
+                song:'什么歌曲',
+                vote:822,
+                followed:false
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:821,
+                followed:true
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:821,
+                followed:false
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:821,
+                followed:false
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:821,
+                followed:false
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:821,
+                followed:false
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:821,
+                followed:false
+            },
+            {
+                name:'定积分',
+                school:'北京是大学',
+                region:'北京赛区',
+                song:'什么歌曲',
+                vote:821,
+                followed:false
+            }
+        ]
+    },
+    handleBeforeCity(e) {
+        console.log('handleBeforeCity')
+    },
+    handleBackCity(e) {
+        console.log('handleBackCity')
+    },
+    handleSelectCity(e) {
+        console.log('handleSelectCity : ', e.target.dataset.city)
+    },
+    handleFollowed(e) {
+        console.log('handleFollowed : ', e.target.dataset.rank)
+    },
+    handleNotFollowed(e) {
+        console.log('handleNotFollowed : ', e.target.dataset.rank)
+    },
+    handleVote(e) {
+        console.log('handleVote : ', e.target.dataset.rank)
+    },
 
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
