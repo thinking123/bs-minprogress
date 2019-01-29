7a26aee48a643c7cc28e496fe2259ce8a9bb4a4c
7ab_100009078201_ThinKing <7ab_100009078201_ThinKing@git.cloud.tencent.com>
2019-01-29 11:23:41 +0800

    win info page UI

---
+++ pages/win-info/index.js
@@ -0,0 +1,54 @@
+// pages/my-achieve/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        achieveList:[
+            {
+                name:'我为原创代言',
+                time:"2019.3.2",
+                hadGot:false
+            },
+            {
+                name:'我为原创代言',
+                time:"2019.3.2",
+                hadGot:true
+            },
+            {
+                name:'我为原创代言',
+                time:"2019.3.2",
+                hadGot:true
+            },
+            {
+                name:'我为原创代言',
+                time:"2019.3.2",
+                hadGot:false
+            },
+            {
+                name:'我为原创代言',
+                time:"2019.3.2",
+                hadGot:false
+            },
+            {
+                name:'我为原创代言',
+                time:"2019.3.2",
+                hadGot:true
+            },
+            {
+                name:'我为原创代言',
+                time:"2019.3.2",
+                hadGot:false
+            },
+            {
+                name:'我为原创代言',
+                time:"2019.3.2",
+                hadGot:false
+            }
+        ]
+    },
+    handleReturn() {
+        console.log('handleReturn')
+    }
+})
