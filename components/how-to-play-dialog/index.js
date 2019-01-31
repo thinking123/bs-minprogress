09b57e4aa03ba97b99e8e30d011badb4401223b0
7ab_100009078201_ThinKing <7ab_100009078201_ThinKing@git.cloud.tencent.com>
2019-01-31 18:43:00 +0800

    UI fix and my music jurtery page

---
+++ components/how-to-play-dialog/index.js
@@ -0,0 +1,42 @@
+const computedBehavior = require('miniprogram-computed')
+const app = getApp()
+const baseComponentUrl = app.globalData.baseComponentUrl
+const component = 'dialog-wrap/'
+// const url = `${baseComponentUrl}${component}`
+const url = './'
+
+Component({
+
+    behaviors: [computedBehavior],
+
+    properties: {
+        visible: {
+            type: Boolean,
+            value: false,
+            observer(newVal, oldVal, changedPath) {
+                console.log('visible change ', newVal, oldVal, changedPath)
+            }
+        },
+        bg: String
+    },
+    data: {
+        url:url
+    },
+    computed: {
+        src() {
+            console.log('src' , this.data.bg)
+            return `${url}${this.data.bg}.png`
+        }
+    },
+    methods: {
+        handleHide() {
+            this.triggerEvent('hidetap')
+        },
+        handleOutHide(e) {
+            if (e && e.target && e.target.id === 'mask') {
+                this.triggerEvent('hidetap')
+            }
+        }
+    }
+
+})
---
+++ pages/music-journey/index.js
@@ -0,0 +1,23 @@
+const app = getApp()
+const baseUrl = app.globalData.baseUrl
+const page = 'home/'
+// const url = `${baseUrl}${page}`
+const url = './'
+Page({
+    data: {
+        baseUrl:url,
+        showDialog:false
+    },
+    handleHowToPlay: function () {
+        console.log('handleRegister')
+        this.setData({
+            showDialog:true
+        })
+    },
+    handleStartMyMusicJourney: function () {
+        console.log('handleMyMusic')
+        // wx.navigateTo({
+        //     url:'/pages/my-music-personality/index'
+        // })
+    }
+})
