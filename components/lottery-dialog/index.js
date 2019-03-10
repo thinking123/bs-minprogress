a694ae93bafb3e444d98f55028d9572cc66e47a2
tk <1294873004@qq.com>
2019-03-10 19:12:34 +0800

    ui

--- components/lottery-dialog/index.js
+++ components/lottery-dialog/index.js
@@ -16,11 +16,20 @@ Component({
             type: Boolean,
             value: false,
             observer(v) {
+                console.log('show' , this.data.lotterying)
                 if(!v){
+
                     this.setData({
-                        lotterying: false
+                        lotterying: false,
+                        isLottering: false,
                     })
+                    clearTimeout(this.time)
+
                 }else{
+                    this.setData({
+                        lotterying: false,
+                        isLottering: false,
+                    })
                     prizeImg().then(prizeImgs => {
                         console.log('lotteryInfo' , prizeImgs)
                         this.setData({
@@ -36,23 +45,41 @@ Component({
     data: {
         url: base,
         lotterying: false,
-        prizeImgs:[]
+        isLottering:false,
+        prizeImgs:[],
+        animationData:{}
+    },
+    detached(){
+      clearTimeout(this.time)
     },
     methods: {
         handleHide() {
-            this.triggerEvent('hidetap')
+            !this.data.isLottering && this.triggerEvent('hidetap')
         },
         handleOutHide(e) {
             if (e && e.target && e.target.id === 'mask') {
-                this.triggerEvent('hidetap')
+                !this.data.isLottering && this.triggerEvent('hidetap')
             }
+        },
+        handleStart(){
+            if(this.data.isLottering || this.data.lotterying){
+                console.log('isLottering' , this.data.isLottering)
+                console.log('lotterying' , this.data.lotterying)
+                return
+            }
+            this.setData({
+                lotterying: true,
+                isLottering:true
+            })
+          this.time = setTimeout(()=>{
+              this.setData({
+                  isLottering:false
+              })
+              this.handleLottery()
+          } , 1000)
         },
         async handleLottery() {
             try {
-                this.setData({
-                    lotterying: true
-                })
-
                 //prizeImage 为空就是没有奖品
                 const prize = await luckDraw(
                     this.data.gid ,
